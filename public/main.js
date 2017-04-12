41bf6bbcd31f8ae02d64703047d7b4ccb187aa5e
Harry Coulson <coulson.mail@me.com>
2017-04-12 14:56:34 -0700

    Troubleshooting details view #2

--- public/main.js
+++ public/main.js
@@ -14,7 +14,6 @@ cars.then(function(response) {
 
 //list-view page
 function renderCar(car) {
-  // var $row = document.createElement('div')//1
   var $column = document.createElement('div')//2
   var $thumbnailClass = document.createElement('div')//3
   var $carPhoto = document.createElement('img')//4
@@ -22,29 +21,30 @@ function renderCar(car) {
   var $carName = document.createElement('h3')//6
   var $description = document.createElement('p')//7
   var $buttonDiv = document.createElement('p')//8
-  var $buttonOne = document.createElement('a')//9
+  var $button = document.createElement('a')//9
 
   $carName.textContent = car.make + ' ' + car.model//6
   $description.textContent = car.description//7
-  $buttonOne.textContent = 'Details'
+  $button.textContent = 'Details'
 
-  // $row.classList.add('row')//1
   $column.classList.add('col-sm-6', 'col-md-4')//2
   $thumbnailClass.classList.add('thumbnail')//3
   $caption.classList.add('caption')//5
-  $buttonOne.classList.add('btn', 'btn-primary','btn', 'btn-default')//9
+  $button.classList.add('btn', 'btn-primary','btn', 'btn-default', 'details')//9
 
   $carPhoto.setAttribute('src', car.photo)//4
-  $buttonOne.setAttribute('href', '#')
+  $carPhoto.setAttribute('id', car.id)
+  $button.setAttribute('href', '#')//9
+  $button.setAttribute('id', car.id)
+  $carName.setAttribute('id', car.id)
 
-  // $row.appendChild($column)
   $column.appendChild($thumbnailClass)
   $thumbnailClass.appendChild($carPhoto)
   $thumbnailClass.appendChild($caption)
   $caption.appendChild($carName)
   $caption.appendChild($description)
   $caption.appendChild($buttonDiv)
-  $buttonDiv.appendChild($buttonOne)
+  $buttonDiv.appendChild($button)
 
   return $column
 
@@ -52,4 +52,21 @@ function renderCar(car) {
     var $listView = document.querySelector('list-view')
     $listView.appendChild(renderCar(car))
   })
+}
+
+// <div class="media">
+//   <div class="media-left">
+//     <a href="#">
+//       <img class="media-object" src="..." alt="...">
+//     </a>
+//   </div>
+//   <div class="media-body">
+//     <h4 class="media-heading">Media heading</h4>
+//     ...
+//   </div>
+// </div>
+
+//details page
+function renderSelectedCar(car) {
+
 }
