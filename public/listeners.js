3c5da5ae3158134cb737f3f05a92811ca61bf5da
Harry Coulson <coulson.mail@me.com>
2017-04-13 09:22:26 -0700

    Troubleshooting click listener #2

--- public/listeners.js
+++ public/listeners.js
@@ -1,1 +1,26 @@
+document.addEventListener('click', function(event) {
+  if(event.target.classList.contains('details')){
 
+  var $clickedItemId = event.target.id
+
+  function getCar(cars) {
+    var $clickedCar = []
+    var $listView = document.querySelector('#list-view.container')
+    cars.forEach(function (car) {
+      if(car.id === $clickedItemId) {
+        if($listView !== null) {
+          $listView.setAttribute('class', 'hidden')
+        }
+        $clickedCar.push(car)
+      }
+    })
+    return $clickedCar
+  }
+
+  var clickedCar = getCar(cars)
+  var $matchedCar = renderSelectedCar(clickedCar[0])
+  var $carDetails = document.querySelector('#car-details')
+  $carDetails.innerHTML=''
+  $carDetails.appendChild($matchedCar)
+  }
+})
